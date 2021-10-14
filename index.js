f6b96c4e244fc8345c0eeda0b6876b0b798156b7
Landon Skinner <landonjskinner@gmail.com>
2021-10-14 11:46:17 -0500

    commit tree c3cc2fe063166cb3fbf40b35e12d1147b3280f1c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(function (names) {
+        return names.toLowerCase().indexOf(name.toLowerCase()) !== -1
+    })
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter(function(name) {
+       return name.startsWith(str);
+    })
+}
+
+function matchName(array, str) {
+    return array.filter(function(driver, name) {
+        return driver.name === str;
+    })
+}
